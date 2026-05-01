20c746612bc477f3a00a1809653b66ea640b83d0
01ice <165007414+01ice@users.noreply.github.com>
2026-05-01 21:47:53 +0800

    第一个代码

---
+++ hello/helloworld/test.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+void test01()
+{
+	char arr1[] = "abc";
+	char arr2[] = {'a','b','c'};
+	printf("%s\n", arr1);
+	printf("%s\n", arr2);
+}
+
+int main()
+{
+	printf("hello world!\n");
+	//test01();
+	//getchar();
+	return 0;
+}
