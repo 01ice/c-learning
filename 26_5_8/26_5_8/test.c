7e977c23dd6e31bfce0f492f19644dbf64a902dd
01ice <165007414+01ice@users.noreply.github.com>
2026-05-08 22:22:21 +0800

    c语言数据类型和变量

---
+++ 26_5_8/26_5_8/test.c
@@ -0,0 +1,61 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main() {
+//    //// 如果下面输出负数，说明char默认为signed
+//    ///*char c = 255;
+//    //printf("%d\n", c);
+//    //return 0;*/
+//    //signed char s = 127;  // char最大值
+//    //s = s + 1;  // 溢出！这是未定义行为
+//    //// 理论上程序可能崩溃、输出任意值、或看似"正常"地变成-128
+//    //printf("%d\n", s);
+//
+//    //unsigned char u = 255; // unsigned char最大值
+//    //u = u + 1;  // 溢出，但行为定义明确
+//    //printf("%u\n", u);  // 保证输出 0（环绕）
+//
+//  /*  while (1)
+//    {
+//        int i = 0;
+//        scanf("%d", &i);
+//        if (i)
+//        {
+//            printf("真\n");
+//        }
+//        else
+//        {
+//            printf("假\n");
+//        }
+//    }*/
+//
+////#include <stdbool.h>
+////    _Bool a = true;
+////    bool b = false;
+////    if (a)
+////        printf("真\n");
+//
+//    //short a = 2;
+//    //int b = 10;
+//    //printf("%zu\n", sizeof(a = a + b));//最后赋给a,a的类型是short,所以输出2
+//    //printf("%d\n", a);//不会影响a,a等于2
+//    return 0;
+//}
+// Linux x86_64 输出 -1（有符号）
+// ARM 某些环境输出 255（无符号）
+
+
+//int a = 10;
+//int main()
+//{
+//	int a = 5;
+//	printf("%d\n", a);
+//	return 0;
+//}
+
+int a = 100;
+extern int a;//声明外部符号
+int main()
+{
+	printf("%d\n", a);
+	return 0;
+}
--- hello/helloworld/test.c
+++ hello/helloworld/test.c
@@ -1,7 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
 
-void test01()
+void test01()//有\0和无\0的区别
 {
 	char arr1[] = "abc";
 	char arr2[] = {'a','b','c'};
